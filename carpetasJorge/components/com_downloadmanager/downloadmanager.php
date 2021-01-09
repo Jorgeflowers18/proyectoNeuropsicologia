5cb94683076011c686d4ea0f6f7b0c0982696d28
Jorgeflowers18 <jefo20156@gmail.com>
2021-01-08 22:09:39 -0500

    Jorge Sprint 3

---
+++ carpetasJorge/components/com_downloadmanager/downloadmanager.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @version     1.5.1
+ * @package     com_downloadmanager
+ * @copyright   Copyright (C) 2017. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Shaon <support@jdownloadmanager.com> - http://jdownloadmanager.com
+ */
+
+
+defined('_JEXEC') or die('Restricted access');
+
+require_once  JPATH_COMPONENT . '/helpers/functions.php';
+JLoader::register('DownloadManagerHelper', JPATH_COMPONENT . '/helpers/downloadmanager.php');
+
+
+//load classes
+JLoader::registerPrefix('DownloadManager', JPATH_COMPONENT);
+
+//Load plugins
+JPluginHelper::importPlugin('downloadmanager');
+
+//application
+$app = JFactory::getApplication();
+
+// Require specific controller if requested
+$controller = $app->input->get('controller','default');
+
+// Create the controller
+$classname  = 'DownloadManagerControllers'.ucwords($controller);
+$controller = new $classname();
+
+
+
+// Perform the Request task
+$controller->execute();
+ 
---
+++ carpetasJorge/components/com_downloadmanager/views/category/html.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @version     1.5.1
+ * @package     com_downloadmanager
+ * @copyright   Copyright (C) 2017. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Shaon <support@jdownloadmanager.com> - http://jdownloadmanager.com
+ */
+
+
+defined('_JEXEC') or die('Restricted access');
+
+
+class DownloadManagerViewsCategoryHtml extends JViewHtml
+{
+    public $task;
+
+    function render()
+    {
+        //display
+        return parent::render();
+    }
+}
