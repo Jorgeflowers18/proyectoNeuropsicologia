359f6e3f80220907404911ab6371830a4f6ecd12
vysery98 <luispsi98@gmail.com>
2021-01-11 11:19:06 -0500

    login

---
+++ carpetasLuis/components/com_downloadmanager/controllers/edit.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version     1.5.1
+ * @package     com_downloadmanager
+ * @copyright   Copyright (C) 2017. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Shaon <support@jdownloadmanager.com> - http://jdownloadmanager.com
+ */
+
+
+defined('_JEXEC') or die('Restricted access');
+ 
+class DownloadManagerControllersEdit extends DownloadManagerControllersDefault
+{
+  function execute()
+  {
+    $app = JFactory::getApplication();
+    $viewName = $app->input->get('view');
+    $app->input->set('layout','edit');
+    $app->input->set('view', $viewName);
+     
+    //display view
+    return parent::execute();
+  }
+}
