6776ae91d78df25062bf8e82eb3617ae5346f1c6
vysery98 <luispsi98@gmail.com>
2021-01-12 02:59:55 -0500

    Versionamiento JOOMLA- Hosting Virtual

--- Core/Model/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect ("localhost", "root", "", "proyectoneuro");
-if($con){
-    echo "Se conectó.";
-}else{
-    echo "No se conectó.";
-}
-//mysql_query ("SET NAME 'utf8'");
